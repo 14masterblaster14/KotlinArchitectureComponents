255cc0ccebb69c8157b94f06438509607f51e940
MasterBlaster <14masterblaster14@gmail.com>
2018-11-12 10:06:35 +0530

    Cleanup

--- app/src/main/java/com/example/admin/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.admin.myapplication
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
+// https://android.jlelse.eu/android-architecture-components-a-complete-kotlin-walkthrough-d59145a14bef
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
